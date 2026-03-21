0a9c88010f8f3bea8e4f4c5193e8b1d8e60b6dc8
bayurifa <91443870+bayurifa@users.noreply.github.com>
2026-03-21 21:06:06 +0700

    Create FID262103.js

---
+++ FID262103.js
@@ -0,0 +1,3 @@
+const firebaseConfig={apiKey:"AIzaSyCC-5llQ0CpexWaUzqDT-qg4WDBOpdVjho",databaseURL:"https://bsr-mobile-bb9d6-default-rtdb.asia-southeast1.firebasedatabase.app"};
+firebase.initializeApp(firebaseConfig);
+const db=firebase.database();
